83a1fc03edcb477cef46c835ffd97f88b10a863b
NikitaShyshkov <super5nikitos@gmail.com>
2023-02-20 11:19:11 +0100

    Definition magazine

---
+++ gitLibrary/Magazine.cpp
@@ -0,0 +1,36 @@
+#include "Magazine.h"
+
+Magazine::Magazine()
+{
+}
+
+Magazine::Magazine(string name, string author, int year):Edition(name, author, year)
+{
+}
+
+void Magazine::addArticle(string article)
+{
+    this->articles.push_back(article);
+}
+
+void Magazine::showArticles() const
+{
+	for (int i = 0; i < articles.size(); i++)
+	{
+		cout << articles[i] << endl;
+	}
+}
+
+void Magazine::showInfo() const
+{
+    cout << type() << ": " << name << endl;
+    cout << "\tby " << author << endl;
+    cout << "Year: " << year << endl;
+    cout << "Articles: " << endl;
+    showArticles();
+}
+
+string Magazine::type() const
+{
+    return "Magazine";
+}
