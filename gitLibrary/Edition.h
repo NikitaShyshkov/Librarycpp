7038efd14ef1d149d84f9bcdf91a2135ed4391fd
NikitaShyshkov <super5nikitos@gmail.com>
2023-02-20 10:41:01 +0100

    Class Edition

---
+++ gitLibrary/Edition.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+using std::string;
+class Edition
+{
+protected:
+	string name;
+	string author;
+	int year;
+public:
+	Edition();
+	Edition(string name, string author, int year);
+
+	void setName(string name);
+	void setAuthor(string author);
+	void setYear(string year);
+
+	string getName()const;
+	string getAuthor()const;;
+	int getYear()const;
+
+	virtual void showInfo()const = 0;
+};
+
