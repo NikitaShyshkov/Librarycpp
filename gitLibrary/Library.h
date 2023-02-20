e8914180d3db25eafb4b9b1f3148e6a00329831a
NikitaShyshkov <super5nikitos@gmail.com>
2023-02-20 16:20:28 +0100

    Add class Library

---
+++ gitLibrary/Library.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Book.h"
+#include "Edition.h"
+#include "Magazine.h"
+class Library
+{
+protected:
+	string name;
+	vector<Edition*> library;
+public:
+	Library();
+	Library(string name);
+
+	void setName(string name);
+	string getName()const;
+
+	void add(Edition* obj);
+	void del(int id);
+	
+	void findByName(string name)const;
+	void findByAuthor(string author)const;
+	void findByYear(int year)const;
+
+	void showList()const;
+};
+
--- gitLibrary/Source.cpp
+++ gitLibrary/Source.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "Book.h"
+#include "Magazine.h"
 
 int main()
 {
+	/*Book a;
+	Magazine b;
+	a.showInfo();
+	cout << "\n\n";
+	b.addArticle("Article 1");
+	b.addArticle("Article 2");
+	b.addArticle("Article 3");
+	b.showInfo();*/
+
 	return 0;
 }
